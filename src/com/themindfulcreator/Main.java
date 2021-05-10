84858c12162679301ea414152e6618135b6d7d40
themindfulcreator <themindfulcreator@gmail.com>
2021-05-10 22:48:23 +0800

    Initial Commit: This piece code is about Hello World written in JAVA (programming language).

---
+++ src/com/themindfulcreator/Main.java
@@ -0,0 +1,10 @@
+package com.themindfulcreator;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This piece code is about Hello World written in JAVA (programming language).
+        String comment = " This piece of code is written in JAVA (programming language).";
+        System.out.println("Hello World!" + comment);
+    }
+}
